5ab0713d26f3bfc8bcb0ee10b5796c5e0d0b15ef
geldenhuys.lynne@gmail.com <geldenhuys.lynne@gmail.com>
2022-04-04 18:57:21 +0200

    Individual property pages complete

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { Box, Flex, Text, Avatar } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/layout';
+import { Avatar } from '@chakra-ui/avatar';
 import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
 
-import DefaultImg from '../assets/images/house.jpg';
+import DefaultImage from '../assets/images/house.jpg';
 
 const Property = ({
   property: {
@@ -27,15 +28,11 @@ const Property = ({
       flexWrap="wrap"
       w="420px"
       p="5"
-      paddingTop="0"
+      paddingTop="0px"
       justifyContent="flex-start"
       cursor="pointer">
       <Box>
-        <Image
-          src={coverPhoto ? coverPhoto.url : DefaultImg}
-          width={400}
-          height={260}
-          alt="house"></Image>
+        <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} />
       </Box>
       <Box w="full">
         <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
@@ -44,7 +41,7 @@ const Property = ({
               {isVerified && <GoVerified />}
             </Box>
             <Text fontWeight="bold" fontSize="lg">
-              AED{millify(price)}
+              AED {price}
               {rentFrequency && `/${rentFrequency}`}
             </Text>
           </Flex>
@@ -52,12 +49,11 @@ const Property = ({
             <Avatar size="sm" src={agency?.logo?.url}></Avatar>
           </Box>
         </Flex>
-        <Flex alignItems="center" p="1" justifyContent="space-between" w="250px" color="gray.500">
+        <Flex alignItems="center" p="1" justifyContent="space-between" w="250px" color="blue.400">
           {rooms}
-          <FaBed /> | {baths}
-          <FaBath /> | {millify(area)}sqft <BsGridFill />
+          <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
         </Flex>
-        <Text fontSize="lg">{title.length > 30 ? `${title.substring(0, 30)}...` : title}</Text>
+        <Text fontSize="lg">{title.length > 30 ? title.substring(0, 30) + '...' : title}</Text>
       </Box>
     </Flex>
   </Link>
